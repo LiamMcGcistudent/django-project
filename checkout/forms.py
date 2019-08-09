1c758bac983a182371bf0dc6b9a4815ef7a15c9a
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-08-09 15:37:38 +0000

    added css for bootstrap styling; styling for reviews pages; trying to find solution to stripe error

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,7 +3,7 @@ from .models import Order
 
 
 class MakePaymentForm(forms.Form):
-    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036 )]
 
     credit_card_number = forms.CharField(label='Credit Card Number', required=False)
@@ -15,6 +15,6 @@ class MakePaymentForm(forms.Form):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'country', 'phone_number', 'country', 'postcode',
+        fields = ('full_name', 'country', 'phone_number', 'postcode',
             'town_or_city', 'street_address1', 'street_address2',
             'county')
