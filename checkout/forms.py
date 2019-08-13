fd09407367cb0b8e157a04a69347be30b9250add
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-08-13 15:25:28 +0000

    issue with stripe still present, will come back to issue at a later time

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,7 +7,7 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2019, 2036 )]
 
     credit_card_number = forms.CharField(label='Credit Card Number', required=False)
-    cvv = forms.CharField(label='Security Code(CVV)')
+    cvv = forms.CharField(label='Security Code(CVV)', required=False)
     expiry_year = forms.ChoiceField(label='year', choices=YEAR_CHOICES, required=False)
     expiry_month = forms.ChoiceField(label='month', choices=MONTH_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
