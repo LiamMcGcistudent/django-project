0c8c4e49ee105d42e51946255ee12f7b212be658
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-09-09 14:51:31 +0000

    commented out products url in base url file to try to fix error

--- confectionerycastle/urls.py
+++ confectionerycastle/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-    path('products/', include('products.urls')),
+    #path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('search/', include ('search.urls')),
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from products.models import Product
+
 
 def index(request):
     """A view that displays the index page"""
     
-    return render(request, "index.html")
+    return render(request, "index.html", {"products":products})
