2b5bda32858dcc268efbe6af7c33354ae55ddd26
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-09-10 15:30:35 +0000

    added new products; fixed bug with database preventing app from loading; styling changes to base.html and index.html;

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .models import Product
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
-    paginator = Paginator(products, 5)
+    paginator = Paginator(products, 3)
     page = request.GET.get('page')
     products = paginator.get_page(page)
     return render(request, "products.html", {"products": products})
