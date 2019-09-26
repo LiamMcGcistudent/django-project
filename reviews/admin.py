07480ecba71e97999041894c94e01628352498b8
Liam McGourty <lmcgourty01@qub.ac.uk>
2019-09-26 15:15:26 +0000

    working on error with review author not showing

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Review, Comment
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['author', 'title', 'content', 'published_date']
+    list_display = ['written_by', 'title', 'content', 'published_date']
 
 admin.site.register(Review)
 admin.site.register(Comment)
